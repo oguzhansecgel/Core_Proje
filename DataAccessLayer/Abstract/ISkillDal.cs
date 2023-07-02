be5f81a9e393f5d32c78991bd5036867db338a08
oguzhansecgel <oguzhansecgel19@icloud.com>
2023-07-02 21:29:01 +0300

    Tema Eklendi,partialview kullanımı, navbar partial, viewcomponents eklendi. Entitylerin listi oluşturulmaya başlandı.

--- DataAccessLayer/Abstract/ISkillDal.cs
+++ DataAccessLayer/Abstract/ISkillDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    internal interface ISkillDal : IGenericDal<Skill>
+    public interface ISkillDal : IGenericDal<Skill>
     {
        
     }
