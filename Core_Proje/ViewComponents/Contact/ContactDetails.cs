f633116c66354d64543c818fc821b5a3487b28cf
oguzhansecgel <oguzhansecgel19@icloud.com>
2023-07-03 00:50:44 +0300

    Veri tabanından gerekli bilgiler çekilmeye devam edildi.

---
+++ Core_Proje/ViewComponents/Contact/ContactDetails.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Contact
+{
+    public class ContactDetails : ViewComponent
+    {
+
+		public IViewComponentResult Invoke()
+		{
+			
+
+			return View();
+		}
+	}
+}
---
+++ Core_Proje/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Core_Proje.ViewComponents.Experience
+{
+    public class ExperienceList : ViewComponent
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = experienceManager.TGetList();
+
+            return View(values);
+        }
+    }
+}
